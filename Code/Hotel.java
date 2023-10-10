2d9d3c7d86690374466551e136f4a3dae974cc37
lmt0813 <lance-matthew.trinidad.462@my.csun.edu>
2023-10-10 15:11:44 -0700

    first Iteration

---
+++ Code/Hotel.java
@@ -0,0 +1,23 @@
+public class Hotel {
+    protected int roomCapacity;
+    protected String location;
+    protected String name;
+    protected String[] staff = new String[];
+    protected String[] criteria = new Stringp[];
+
+    protected void hireStaff(){
+
+    }
+
+    protected void takesPayment(){
+
+
+    }
+
+    protected void providesService(){
+
+
+    }
+
+    
+}
