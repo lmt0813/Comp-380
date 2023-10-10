4477a5ad748bdadec97004df1d74ef02b4b0412f
lmt0813 <lance-matthew.trinidad.462@my.csun.edu>
2023-10-10 15:16:12 -0700

    constructors added

--- Code/Hotel.java
+++ Code/Hotel.java
@@ -1,9 +1,20 @@
 public class Hotel {
     protected int roomCapacity;
     protected String location;
-    protected String name;
+    protected String hotelName;
     protected String[] staff = new String[];
     protected String[] criteria = new Stringp[];
+
+    public Hotel(){
+
+    }
+
+    public Hotel(String hotelName, String location, String[] staff, String[] critera){
+        this.hotelName = hotelName;
+        this.staff = staff;
+        this.criteria = critera;
+        this.location = location;
+    }
 
     protected void hireStaff(){
 
