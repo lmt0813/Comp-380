50f8129933b61a71ca86ab92396d10d2e057253a
joeykaz27 <joseph.kaz.911@my.csun.edu>
2023-10-17 15:48:36 -0700

    Updated requirements for the create account class

--- Code/CreateAccount.java
+++ Code/CreateAccount.java
@@ -3,8 +3,9 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.io.*;
-
 
 //Problems  spamming create Account button on GUI will create that account that many times, so
 //          the newUsers.txt and usernames.txt will have that many duplicates
@@ -18,22 +19,24 @@ import java.io.*;
 //Fix:      easy fix, just do it
 //You can delete these when the problems are fixed ^^ I leave it to you Joey
 
-public class CreateAccount extends JFrame implements ActionListener {
+public class CreateAccount extends JFrame implements ActionListener, ItemListener {
     
     JFrame frame;
-    String userName, name, password, accountType, address, phoneNumber;
+    String userName, name, password, accountType, address, phoneNumber, type, email;
     char passwordResult[];
-    JLabel userNameLabel, nameLabel, passwordLabel, addressLabel, phoneNumberLabel, subPhoneLabel;
+    JLabel userNameLabel, nameLabel, passwordLabel, addressLabel, phoneNumberLabel, subPhoneLabel, typeLabel, emailLabel;
     JPasswordField passwordField;
     String fields[];
-    JTextField userNameField, nameField, addressField, phoneNumberField;
+    JTextField userNameField, nameField, addressField, phoneNumberField, emailField;
     JButton createButton;
     BufferedWriter bw;
     File userFile;
+    JComboBox<String> cb;
     static ArrayList<String> usernameList;
 
 
     CreateAccount() {
+        usernameList = new ArrayList<String>();
         frame = new JFrame();
         userNameField = new JTextField();
         nameField = new JTextField();
@@ -51,46 +54,63 @@ public class CreateAccount extends JFrame implements ActionListener {
         phoneNumber = "";
 
         // add data entry fields
+        typeLabel = new JLabel("Enter user type: ");
+        cb = new JComboBox<String>();
+        cb.addItemListener(this);
+        cb.addItem("User");
+        cb.addItem("Manager");
+        typeLabel.setBounds(25,15,100,20);
+        cb.setBounds(250,15,200,20);
+        frame.add(typeLabel);
+        frame.add(cb);
+
+        emailLabel = new JLabel("Enter Email");
+        emailField = new JTextField();
+        emailLabel.setBounds(25,65,100,20);
+        emailField.setBounds(250,65,200,20);
+        frame.add(emailLabel);
+        frame.add(emailField);
+
         nameLabel = new JLabel("Enter your name:");
-        nameLabel.setBounds(25,15,100,20);
-        nameField.setBounds(250, 15, 200, 20);
+        nameLabel.setBounds(25,115,100,20);
+        nameField.setBounds(250, 115, 200, 20);
         frame.add(nameLabel);
         frame.add(nameField);        
 
         userNameLabel = new JLabel("Enter Username:");
-        userNameLabel.setBounds(25,65,100,20);
-        userNameField.setBounds(250,65,200,20);
+        userNameLabel.setBounds(25,165,100,20);
+        userNameField.setBounds(250,165,200,20);
         frame.add(userNameLabel);
         frame.add(userNameField);
 
         passwordLabel = new JLabel("Enter Password:");
-        passwordLabel.setBounds(25, 115, 100, 20);
-        passwordField.setBounds(250, 115, 200, 20);
+        passwordLabel.setBounds(25, 215, 100, 20);
+        passwordField.setBounds(250, 215, 200, 20);
         frame.add(passwordLabel);
         frame.add(passwordField);
         
         addressLabel = new JLabel("Enter Address:");
-        addressLabel.setBounds(25, 165, 100, 20);
-        addressField.setBounds(250, 165, 200, 20);
+        addressLabel.setBounds(25, 265, 100, 20);
+        addressField.setBounds(250, 265, 200, 20);
         frame.add(addressLabel);
         frame.add(addressField);
 
         phoneNumberLabel = new JLabel("Enter Phone Number:");
-        phoneNumberLabel.setBounds(25, 215, 150, 20);
-        phoneNumberField.setBounds(250, 215, 200, 20);
+        phoneNumberLabel.setBounds(25, 315, 150, 20);
+        phoneNumberField.setBounds(250, 315, 200, 20);
         subPhoneLabel = new JLabel("Format must be: ###-###-####");
-        subPhoneLabel.setBounds(25,240, 175, 20);
+        subPhoneLabel.setBounds(25,335, 175, 20);
         frame.add(phoneNumberLabel);
         frame.add(phoneNumberField);
         frame.add(subPhoneLabel);
 
         createButton = new JButton("Create Account");
-        createButton.setBounds(75,315,300,20);
+        createButton.setBounds(75,450,300,20);
         createButton.addActionListener(this);
         frame.add(createButton);
 
         frame.setTitle("Create Account");
-        frame.setSize(500,500);
+        frame.setSize(500,600);
         frame.setLayout(null);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -98,7 +118,6 @@ public class CreateAccount extends JFrame implements ActionListener {
   
 
     public static void main(String[] args) {
-        usernameList = new ArrayList<String>();
         setUsernameList();
         CreateAccount newAccount = new CreateAccount();
     }
@@ -109,6 +128,13 @@ public class CreateAccount extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         if(e.getSource() == createButton) {
             getAttributes();
+        }
+    }
+
+    @Override
+    public void itemStateChanged(ItemEvent ie) {
+        if(ie.getSource() == cb) {
+            type = cb.getSelectedItem().toString();
         }
     }
 
@@ -128,17 +154,19 @@ public class CreateAccount extends JFrame implements ActionListener {
             JOptionPane.showMessageDialog(null, "username already in use");
             return;
         }
+        type = cb.getSelectedItem().toString();
+        System.out.println(type);
+        email = emailField.getText();
         name = nameField.getText();
         passwordResult = passwordField.getPassword();
         convertPassword();
         address = addressField.getText();
         phoneNumber = phoneNumberField.getText();
-        fields = new String[] {name, userName, password, address, phoneNumber};
+        fields = new String[] {type, email,name, userName, password, address, phoneNumber};
         if(checkNullFields() == false) {
             return;
         }
         if(checkPhoneNumber() == false) return;
-        System.out.println("The method worked and I can go watch football!");
 
         //writing usernames into usernames.txt
         try{ PrintStream ps = new PrintStream(new FileOutputStream("usernames.txt", true));
@@ -154,12 +182,15 @@ public class CreateAccount extends JFrame implements ActionListener {
         } catch (FileNotFoundException e) {}
         
         createAccount();
+        
     }
 
     public boolean checkNullFields() {
+        System.out.println(userName);
         for(int i = 0; i < fields.length; i++) {
-            if(fields[i].isEmpty()) {
+            if(fields[i].compareTo("") == 0) {
                 String emptyField = getEmptyField(i);
+                System.out.println(emptyField);
                 JOptionPane.showMessageDialog(null, emptyField + " cannot be empty!");
                 return false;
             }
@@ -192,14 +223,18 @@ public class CreateAccount extends JFrame implements ActionListener {
     public String getEmptyField(int i) {
         switch(i) {
             case 0:
-                return "Name";
+                return "Type";
             case 1:
-                return "Username";
+                return "Email";
             case 2:
-                return "Password";
+                return "Name";
             case 3:
-                return "Address";
+                return "Username";
             case 4:
+                return "Password";
+            case 5:
+                return "Address";
+            case 6:
                 return "Phone Number";
             default:
                 break;
