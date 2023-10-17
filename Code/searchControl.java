7658dcc78a0c830946433e0568e16aef50be1ec0
lmt0813 <lance-matthew.trinidad.462@my.csun.edu>
2023-10-17 15:47:41 -0700

    first iteration, need to define many things see comments

---
+++ Code/searchControl.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class searchControl {
+     protected ArrayList<String> searchCriteria;
+     
+     public ArrayList<Hotel> searchResults(String[] search){
+        ArrayList<Hotel> results = new ArrayList<Hotel>();
+
+        ArrayList<Hotel> dummyList;
+
+        //assuming that an array of hotels exist and a different class has this
+        // method to pass said list on
+
+        //also need to make getCriteria method in hotel.java
+
+        //dummyList = getHotelList();  
+
+        for(int i = 0; i < dummmyList.length(); i++){
+            for(int j = 0; j < dummyList.get(i).getCriteria.length(); j++){
+                if(dummyList.get(i).getCriteria[j].compareTo(search[i]) == 0){
+                    results.add(dummyList.get(i));
+                }
+            }
+
+        }
+
+        return results;
+     }
+    
+}
