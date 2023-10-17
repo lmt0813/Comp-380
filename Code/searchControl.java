9431b07d8624620cf6dbc4ffcd22b646f76ac3ce
joeykaz27 <joseph.kaz.911@my.csun.edu>
2023-10-17 16:27:22 -0700

    commented out method to stop showing errors

--- Code/searchControl.java
+++ Code/searchControl.java
@@ -4,9 +4,9 @@ public class searchControl {
      protected LinkedList<String> searchCriteria;
      
      public LinkedList<Hotel> searchResults(LinkedList<String> search){
-        ArrayList<Hotel> results = new ArrayList<Hotel>();
+        LinkedList<Hotel> results = new LinkedList<Hotel>();
 
-        ArrayList<Hotel> dummyList;
+        LinkedList<Hotel> dummyList;
 
         //assuming that an array of hotels exist and a different class has this
         // method to pass said list on
@@ -15,6 +15,7 @@ public class searchControl {
 
         //dummyList = getHotelList();  
 
+        /*
         for(int i = 0; i < dummmyList.length(); i++){
             for(int j = 0; j < dummyList.get(i).getCriteria.length(); j++){
                 if(dummyList.get(i).getCriteria[j].compareTo(search[i]) == 0){
@@ -23,6 +24,7 @@ public class searchControl {
             }
 
         }
+        */
 
         return results;
      }
