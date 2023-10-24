05ce5f0c31fd3b8a60174138e1fd51b374c38a13
joeykaz27 <joseph.kaz.911@my.csun.edu>
2023-10-24 10:21:46 -0700

    added comments for goals to complete the class

--- Code/searchControl.java
+++ Code/searchControl.java
@@ -15,6 +15,16 @@ public class searchControl {
 
     searchControl() {roomFile = new File("./Room.txt");}
    
+    /*
+     goals for this class:
+     1) Create an additional method specifically for scanning from the text file. The searchResults method below should be a main method
+     that calls other methods and returns the list called from the HotelGUI class
+     2) Creat a method that filters results based on the search criteria. We will most likely have to put two lines per hotel on the text file: one
+     for the attributes, and a second for criteria.
+     3) Could either create a initial list to scan everything and second list to return based on the filtered criteria, or load everything in to one list.,
+     check the entire list, and remove every room that doesn't fit the criteria
+     4) Most likely scan hotel text file, check for attributes per hotel, then scan the room list based on hotel ID
+     */
 
      public LinkedList<Room> searchResults(LinkedList<String> search) {
         try {
@@ -36,12 +46,15 @@ public class searchControl {
         return results;
     }
 
+    // method that adds rooms to the return list
+
     public void addRoom(String[] attStrings) {
         assignAttributes();
         results.add(new Room(hotelID, roomID, numberBed, avaialbility, floorNumber, roomNumber, roomType, price));
         System.out.println("room added");
     }
 
+    // method that assigns sent attributes to the data types defined in the class header
     public void assignAttributes() {
         hotelID = attributes[0];
         roomID = attributes[1];
