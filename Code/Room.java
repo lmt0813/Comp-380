bd9cd4879ddad14f63949b82ec677f2c1644bf99
Geoffrey Anselmo <geoffrey.anselmo.807@my.csun.edu>
2023-10-25 20:02:05 -0700

    added constructor

--- Code/Room.java
+++ Code/Room.java
@@ -1,14 +1,24 @@
 public class Room {
     protected int numberBed;
     protected boolean avaialbility;
-    protected int keycard;
+    protected String roomID;
     protected int floorNumber;
     protected int roomNumber;
     protected String roomType;
-    protected int ID;
+    protected String hotelID;
+    protected double price;
 
-    public Room() {
+    public Room(String hotelID, String roomID, int numberBed, boolean availability,
+                int floorNumber, int roomNumber, String roomType, double price) {
 
+        this.hotelID = hotelID;
+        this.roomID = roomID;
+        this.numberBed = numberBed;
+        this.avaialbility = availability;
+        this.floorNumber = floorNumber;
+        this.roomNumber = roomNumber;
+        this.roomType = roomType;
+        this.price = price;
     }
 
     protected void assignKeyCard() {
