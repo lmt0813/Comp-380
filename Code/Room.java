b96f37927f7826014838fb39bd4bd40eca910327
joeykaz27 <joseph.kaz.911@my.csun.edu>
2023-10-28 14:24:53 -0700

    Updated Room class constructor

--- Code/Room.java
+++ Code/Room.java
@@ -5,10 +5,10 @@ public class Room {
     protected int floorNumber;
     protected int roomNumber;
     protected String roomType;
-    protected int hotelID;
+    protected String hotelID;
     protected double price;
 
-    public Room(int hotelID, String roomID, int numberBed, boolean availability,
+    public Room(String hotelID, String roomID, int numberBed, boolean availability,
                 int floorNumber, int roomNumber, String roomType, double price) {
 
         this.hotelID = hotelID;
