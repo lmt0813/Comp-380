074c710adca1b1afb9fcf41d87098cc31fa70d8f
veekR <victor1213113@gmail.com>
2023-11-02 16:21:31 -0700

    added validation methods

--- Code/bookingGUI.java
+++ Code/bookingGUI.java
@@ -5,7 +5,6 @@ import java.awt.event.ActionListener;
 
 public class bookingGUI extends JFrame implements ActionListener{
     JLabel hotelLabel, checkinLabel, checkoutLabel, totalLabel, nameLabel, cardLabel, expLabel, securityLabel;
-    //NOT FILLED 
     JLabel hotelNameLabel, checkinDateLabel, checkoutDateLabel, totalFillLabel;
     JButton submitButton;
     JComboBox <String> selectPay;
@@ -18,13 +17,13 @@ public class bookingGUI extends JFrame implements ActionListener{
         bookingGUI(Booking booking){
             frame = new JFrame();
             hotelLabel = new JLabel("Hotel :");
-            //hotelNameLabel = new JLabel(booking.hotel.hotelName);
+            hotelNameLabel = new JLabel(booking.hotel.hotelName);
             checkinLabel = new JLabel("Checkin Data :");
-            //checkinDateLabel = new JLabel(booking.checkInDate);
+            checkinDateLabel = new JLabel(booking.checkInDate);
             checkoutLabel = new JLabel("Checkout Date :");
-            //checkoutDateLabel = new JLabel(booking.checkOutDate);
+            checkoutDateLabel = new JLabel(booking.checkOutDate);
             totalLabel = new JLabel("Total: ");
-            //TotalFillLabel = new JLabel(Double.toString(booking.price));
+            totalFillLabel = new JLabel(Double.toString(booking.price));
             selectPay = new JComboBox<String>();
             nameLabel = new JLabel("Name on card :");
             nameField = new JTextField();
@@ -41,16 +40,16 @@ public class bookingGUI extends JFrame implements ActionListener{
 
             hotelLabel.setBounds(50, 50, 150, 50);
             frame.add(hotelLabel);
-            //hotelNameLabel.setBounds(150, 50, 150, 50);
-            //frame.add(hotelNameLabel);
+            hotelNameLabel.setBounds(150, 50, 150, 50);
+            frame.add(hotelNameLabel);
             checkinLabel.setBounds(50, 100, 150, 50);
             frame.add(checkinLabel);
-            //checkinDateLabel.setBounds(150, 125, 150, 50);
-            //frame.add(checkinDateLabel);
+            checkinDateLabel.setBounds(150, 125, 150, 50);
+            frame.add(checkinDateLabel);
             checkoutLabel.setBounds(50,150, 150, 50);
             frame.add(checkoutLabel);
-            //checkoutDateLabel.setBounds(150, 135, 150, 50);
-            //frame.add(checkoutDateLabel);
+            checkoutDateLabel.setBounds(150, 135, 150, 50);
+            frame.add(checkoutDateLabel);
             totalLabel.setBounds(50, 200, 150, 50);
             frame.add(totalLabel);
             selectPay.setBounds(50, 250, 165, 25);
@@ -109,7 +108,9 @@ public class bookingGUI extends JFrame implements ActionListener{
             return 0;
         }
 
-
+        /*vallidates card
+        checks  for 16 digit card number 
+        */ 
         int validateCard(){
             String cardLength = cardField.getText();
             if (cardLength.length()!= 16){
@@ -128,14 +129,35 @@ public class bookingGUI extends JFrame implements ActionListener{
         int validateExp(){
             String ExpDate = expField.getText();
             if (ExpDate.length() != 5){
-                JOptionPane.showMessageDialog(null, "must proper date format (MM/YY)");
-            }
-            if(!Character.isDigit(0)||!Character.isDigit(1))
-                JOptionPane.showConfirmDialog(null,"must be in MM/YY format");
+                JOptionPane.showMessageDialog(null, "Must be proper date format (MM/YY)");
                 return 1;
-        }//need to push 
+            }
+            if(!Character.isDigit((ExpDate.charAt(0)))||!Character.isDigit(ExpDate.charAt(1))){
+                JOptionPane.showMessageDialog(null,"Must be proper date format (MM/YY)");
+                return 1;
+            }
+            if(ExpDate.charAt(2) != '/'){
+                JOptionPane.showMessageDialog(null, "Must be proper date format (MM/YY)");
+                return 1;
+            }
+            if(!Character.isDigit(ExpDate.charAt(3))||!Character.isDigit(ExpDate.charAt(4))){
+                JOptionPane.showMessageDialog(null, "Must be proper date format (MM/YY)");
+                return 1;
+            }
+            
+            return 0;
+        }
 
         int validateCVV(){
+                String CardCVV = securityField.getText();
+                if (CardCVV.length() != 3){
+                JOptionPane.showMessageDialog(null, "CVV must be 3 numbers");
+                return 1;
+                }
+                if(!Character.isDigit(CardCVV.charAt(0))||!Character.isDigit(CardCVV.charAt(2))||!Character.isDigit(CardCVV.charAt(3))){
+                JOptionPane.showMessageDialog(null, "CVV must contain 3 numbers");
+                return 1;
+                }
                 return 0;
         }
 
