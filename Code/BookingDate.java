6e7c538185974f73229bbaf23c671d9d0eb64278
lmt0813 <lance-matthew.trinidad.462@my.csun.edu>
2023-11-20 18:59:24 -0800

    Added javadoc comments for BookingDate.java and Hotel.java

--- Code/BookingDate.java
+++ Code/BookingDate.java
@@ -3,25 +3,47 @@ public class BookingDate {
 
     LocalDate checkIn, checkOut;
 
+    /**Default constructor for Hotel objects
+    */
     BookingDate(){}
 
+    /**Constructor with parameters for BookingDate objects
+     * @param checkIn the date the user will check into the hotel
+     * @param checkOut the date the user will check out of the hotel
+    */
     BookingDate(LocalDate checkIn, LocalDate checkOut) {
         this.checkIn = checkIn;
         this.checkOut = checkOut;
     }
 
+    
+    /**Changes the check in date 
+     * @param checkIn The check in date you want to be set as
+     */
     public void setCheckIn(LocalDate checkIn) {
         this.checkIn = checkIn;
     }
 
+    
+    /**Changes the check out date 
+     * @param checkOut The check out date you want to be set as
+     */
     public void setCheckOut(LocalDate checkOut) {
         this.checkOut = checkOut;
     }
 
+    
+    /**Returns the check in date
+     * @return LocalDate object containing the check in date
+     */
     public LocalDate getCheckIn() {
         return this.checkIn;
     }
 
+    
+    /**Returns the check out date
+     * @return LocalDate object containing the checkout date
+     */
     public LocalDate getCheckOut() {
         return this.checkOut;
     }
--- Code/Hotel.java
+++ Code/Hotel.java
@@ -8,8 +8,18 @@ public class Hotel {
     protected String[] criteria;
     protected Room[] roomList;
 
+    /**Default constructor for Hotel objects
+    */
     public Hotel() {}
 
+    /**Constructor with parameters for Hotel objects
+     * @param hotelName Name of the hotel
+     * @param location Where the hotel is located
+     * @param roomCapacity The total number of rooms the hotel has
+     * @param staff A list of the staff employed at the hotel
+     * @param criteria A list of criteria tags that the hotel has
+     * @param roomList A list of the rooms in the hotel
+     */
     public Hotel(String hotelName, String location, int roomCapacity, String[] staff, String[] critera, Room[] roomList){
         this.hotelName = hotelName;
         this.staff = staff;
@@ -19,10 +29,18 @@ public class Hotel {
         this.roomList = roomList;
     }
 
+    
+    /**Changes the number of rooms available
+     * @param rooms number of rooms available
+     */
     public void changeVacancy(int rooms){
         this.roomsAvailable = rooms;
     }
 
+    
+    /**Returns the criteria of the hotel 
+     * @return String[] of the criteria assocaited with the hotel 
+     */
     public String[] getCriteria(){
         return this.criteria;
     }
