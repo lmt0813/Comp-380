61515569fb9198d7986549e0ffd6b3956034f942
lmt0813 <lance-matthew.trinidad.462@my.csun.edu>
2023-11-21 18:51:41 -0800

    began MyBookingsGUI.java

---
+++ Code/MyBookingsGUI.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;  
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author Lance Trinidad
+ * @author Geoffrey Anselmo
+ * @version 11/21/2023
+ */
+public class MyBookingsGUI extends JFrame implements ActionListener{
+    JLabel 
+    JFrame
+    MyBookingsGUI(){}
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+    }
+}
