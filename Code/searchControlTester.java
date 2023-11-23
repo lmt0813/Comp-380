0a3bedb74722c982342bcc81a9b34e88c62dc01b
Geoffrey Anselmo <geoffrey.anselmo.807@my.csun.edu>
2023-11-22 19:17:53 -0800

    updated searchControlTester to work with changes in searchControl

--- Code/searchControlTester.java
+++ Code/searchControlTester.java
@@ -1,12 +1,16 @@
 import static org.junit.Assert.assertEquals;
 import java.util.*;
 import java.io.*;
+import java.time.LocalDate;
 
 import org.junit.Test;
 
 
     public class searchControlTester {
-        
+
+        LocalDate checkin = LocalDate.of(2023, 2, 1);
+        LocalDate checkout = LocalDate.of(2023, 2, 14);
+
         @Test
         public void emptySearchbarAndEmptyCriteria() {
             LinkedList<Room> rooms = new LinkedList<>();
@@ -18,9 +22,12 @@ import org.junit.Test;
                     Integer.parseInt(attributes[5]), attributes[6], Double.parseDouble(attributes[7]));
                     rooms.add(room);
                 }
+                rooms.remove(0);
+                rooms.remove(4);
             }
+            
             catch(FileNotFoundException e) {}
-            searchControl sc = new searchControl();
+            searchControl sc = new searchControl(checkin, checkout);
             LinkedList<String> criteria = new LinkedList<>();
             String searchbar = "";
             LinkedList<Room> result = sc.searchResults(criteria,searchbar);
@@ -33,24 +40,10 @@ import org.junit.Test;
         public void emptyCriteria() {
             //1,1-13,2,true,1,13,King,278.00
             //1,1-14,1,true,1,14,Twin,333.00
-            //
             LinkedList<Room> rooms = new LinkedList<>();
-            rooms.add(new Room(1, "1-13", 2, true, 1, 13, "King", 278.00));
-            rooms.add(new Room(1, "1-14", 1, true, 1, 14, "Twin", 333.00));
-            // try {Scanner sc = new Scanner(new File("Room.txt"));
-            //     for(int i = 0; i < 6; i++)  {
-            //         String[] attributes = sc.next().split(",");
-            //         if(i== 0 || i==5) {
-                        
-            //             Room room = new Room(Integer.parseInt(attributes[0]), attributes[1], Integer.parseInt(attributes[2]),
-            //             Boolean.parseBoolean(attributes[3]), Integer.parseInt(attributes[4]),
-            //             Integer.parseInt(attributes[5]), attributes[6], Double.parseDouble(attributes[7]));
-            //             rooms.add(room);
-            //         }
-            //     }
-            // }
-            //catch(FileNotFoundException e) {}
-            searchControl sc = new searchControl();
+            //rooms.add(new Room(1, "1-13", 2, true, 1, 13, "King", 278.00));
+            //rooms.add(new Room(1,"1-14",1,true,1,14,"Twin",333.00));
+            searchControl sc = new searchControl(checkin,checkout);
             LinkedList<String> criteria = new LinkedList<>();
             String searchbar = "The Hilton Inn";
             LinkedList<Room> result = sc.searchResults(criteria,searchbar);
@@ -60,10 +53,10 @@ import org.junit.Test;
         @Test
         public void emptySearchbar() {
             LinkedList<Room> rooms = new LinkedList<>();
-            rooms.add(new Room(1, "1-13", 2, true, 1, 13, "King", 278.00));
-            rooms.add(new Room(1, "1-14", 1, true, 1, 14, "Twin", 333.00));
-            
-            searchControl sc = new searchControl();
+            //rooms.add(new Room(1, "1-13", 2, true, 1, 13, "King", 278.00));
+            rooms.add(new Room(4,"4-212",1,true,2,212,"Standard",62.00));
+            //rooms.add(new Room(1, "1-14", 1, true, 1, 14, "Twin", 333.00));
+            searchControl sc = new searchControl(checkin, checkout);
             LinkedList<String> criteria = new LinkedList<>();
             criteria.add("Pool");
             String searchbar = "";
@@ -76,7 +69,7 @@ import org.junit.Test;
             LinkedList<Room> rooms = new LinkedList<>();
           
             rooms.add(new Room(2, "2-465", 2, true, 4, 465, "Queen", 127.00));
-            searchControl sc = new searchControl();
+            searchControl sc = new searchControl(checkin, checkout);
             LinkedList<String> criteria = new LinkedList<>();
             criteria.add("Pet Friendly");
             String searchbar = "Ramada Inn";
@@ -93,10 +86,10 @@ import org.junit.Test;
             rooms.add(new Room(3, "3-237", 2, true, 2, 237, "Standard", 79.00));
            //dunno what expected value should be
            //should it return all Rooms under Pet Friendly or nothing at all since it doesn't match searchbar?
-            searchControl sc = new searchControl();
+            searchControl sc = new searchControl(checkin, checkout);
             LinkedList<String> criteria = new LinkedList<>();
             criteria.add("Pet Friendly");
-            String searchbar = "d";
+            String searchbar = "dsadhfjldshfj";
             LinkedList<Room> result = sc.searchResults(criteria,searchbar);
             assertEquals(rooms.toString(), result.toString());
         }
