fc9016a7f34301100f372d9d90f072ccdde53ab2
joeykaz27 <joseph.kaz.911@my.csun.edu>
2023-11-30 14:08:47 -0800

    sorry Geoffrey

--- Code/bookingGUI.java
+++ Code/bookingGUI.java
@@ -31,10 +31,10 @@ public class bookingGUI extends JFrame implements ActionListener{
         /**Constructor with parameters for bookingGui
          * @param booking the booking that the user wants 
          */
-        bookingGUI(Booking booking, JFrame hotelGUIFrame, Account user){
+        bookingGUI(Booking booking, JFrame hotelGUIFrame){
             this.booking = booking;
             this.hotelGUIFrame = hotelGUIFrame;
-            this.user = user;
+            this.user = booking.getAccount();
             frame = new JFrame();
             hotelLabel = new JLabel("Hotel :");
             hotelNameLabel = new JLabel(Integer.toString(booking.hotelID));
